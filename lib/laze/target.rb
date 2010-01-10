1dda02c1cd92c143360ad47f7565cddae23e1bcd
Arjan van der Gaag <info@agwebdesign.nl>
2010-01-10 13:49:08 +0100

    Simplified base class

--- lib/laze/target.rb
+++ lib/laze/target.rb
@@ -1,29 +1,25 @@
 module Laze
-  module Target
-    def self.[](kind, *args)
-      Base[kind, *args]
+  class Target
+    include Enumerable
+
+    TargetException = Class.new(Exception)
+    FileSystemException = Class.new(TargetException)
+
+    @targets = []
+
+    def self.find(kind)
+      targets = @targets.select { |s| s.name.to_s.split('::').last.downcase.to_sym == kind }
+      raise TargetException, 'No such target.' unless targets.any?
+      targets.first
     end
 
-    class Base
-      @children = {}
-      @instances = {}
-
-      class << self
-        def inherited(child)
-          @children[child.name.downcase.split(/::/).last.to_sym] = child
-        end
-
-        def for(kind, *args)
-          @instances[kind] ||= @children[kind].send(:new, *args) rescue NoMethodError raise ChildNotFoundException
-        end
-        alias_method :[], :for
-
-        private :new
-      end
-
-      include Enumerable
+    def self.inherited(child)
+      Laze::LOGGER.debug "Registering target #{child}"
+      @target << child
     end
 
-    ChildNotFoundException = Class.new(Exception)
+    def initialize
+      Laze::LOGGER.debug "Initialized #{self.name}"
+    end
   end
 end
