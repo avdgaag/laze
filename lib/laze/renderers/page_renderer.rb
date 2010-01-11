f0b640dc21622cf7043fed273daf39bfcd6c37f4
Arjan van der Gaag <info@agwebdesign.nl>
2010-01-11 15:09:14 +0100

    Documentation, removing inline stringify_keys

--- lib/laze/renderers/page_renderer.rb
+++ lib/laze/renderers/page_renderer.rb
@@ -3,25 +3,15 @@ module Laze
     # Renders Page objects to an HTML page. This means applying text filters,
     # layouts and the Liquid templating engine to a source file and returning
     # the full HTML result file.
-    #--
-    # TODO: make the text filter to use optional and customizable, like
-    #       markdown, textile, rdoc, etc.
-    # TODO: move text filter into the page class, since its the page's
-    #       responsibility what text filter to use.
     class PageRenderer < Renderer
       def initialize(page) #:nodoc:
         raise ArgumentError unless page.is_a?(Page)
         super(page.filtered_content, page.properties)
       end
 
-      # First apply markdown, only then wrap in layout. A layout that contains
-      # HTML boiler plate stuff (<html>, <doctype>, etc) will get messed up
-      # when markdownized.
+      # Apply layouts and the liquid templating language.
       def render(locals = {})
-        output = string
-        output = wrap_in_layout(output)
-        output = liquify(output, (options[:locals] || {}).merge(locals))
-        output
+        liquify(wrap_in_layout(string), (options[:locals] || {}).merge(locals))
       end
 
     private
@@ -42,17 +32,8 @@ module Laze
         output
       end
 
-      # TODO: take options for output
       def liquify(string, locals = {})
-        Liquid::Template.parse(string).render('page' => stringify_keys(locals))
-      end
-
-      # Convert { :foo => 'bar' } to { 'foo' => 'bar' }
-      def stringify_keys(hash)
-        hash.inject({}) do |options, (key, value)|
-          options[key.to_s] = value
-          options
-        end
+        Liquid::Template.parse(string).render('page' => locals.stringify_keys)
       end
     end
   end
